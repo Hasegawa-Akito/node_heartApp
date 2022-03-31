9f9b9ea085f8a0c96584f17c782c26299317ad70
Hasegawa-Akito <h.akito1999@gmail.com>
2022-03-31 11:28:40 +0900

    create socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const http = require("http");
 const server = http.createServer(app);
 const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-    res.send("HEllo World");
-});
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// 別オリジンからのアクセスを許可する（CORSモジュール利用）
+const cors = require("cors");
+app.use(cors());
+
 
 app.get("/api", (req, res) => {
     res.json({ message: "hello world" });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'../frontend/build/index.html'));
+});
+
+// サーバーオブジェクトsocketioを作成する
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {                      
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
+// ブラウザから接続されたときの処理を定義する
+io.on("connection", (socket) => { // ブラウザから接続されたときの処理
+    console.log("a user connected");
+    socket.on("disconnect", () => { // ブラウザが切断したときの処理
+        console.log("user disconnected");
+    });
 });
 
 server.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react'
+// socket.io-clientをインポートする
+import { io } from "socket.io-client";
 
 function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() =>{
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[]);
+
+  //socket.ioで通信
+  const socket = io("http://localhost:8000");
+  // サーバに接続できた場合のイベント処理を定義する
+  socket.on("connect", () => {
+    console.log(`socket.connectを出力`);
+    console.log(socket.connect()); // サーバに接続できたかどうかを表示
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>フロントエンドnnnn</h1>
+      <p>{ message }</p>
     </div>
   );
 }
