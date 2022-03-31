0cf6b7521ffd6128c94cf236c9799b330d30ac3f
Hasegawa-Akito <h.akito1999@gmail.com>
2022-03-31 17:13:38 +0900

    create Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState,useEffect } from 'react'
 // socket.io-clientをインポートする
 import { io } from "socket.io-client";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -11,20 +12,36 @@ function App() {
       .then((data) => setMessage(data.message));
   },[]);
 
-  //socket.ioで通信
-  const socket = io("http://localhost:8000");
-  // サーバに接続できた場合のイベント処理を定義する
-  socket.on("connect", () => {
-    console.log(`socket.connectを出力`);
-    console.log(socket.connect()); // サーバに接続できたかどうかを表示
-  });
+  function onClickButton(){
+    //socket.ioで通信
+    const socket = io("http://localhost:8000");
+    // サーバに接続できた場合のイベント処理を定義する
+    socket.on("connect", () => {
+      console.log(`socket.connectを出力`);
+      console.log(socket.connect()); // サーバに接続できたかどうかを表示
+    });
+  }
+
+  
 
   return (
-    <div className="App">
-      <h1>フロントエンドnnnn</h1>
-      <p>{ message }</p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>フロントエンドnnnn</h1>
+        <p>{ message }</p>
+        <button type="button" onClick={onClickButton} >close</button>
+      </div>
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+        
+      </Routes>
+    </BrowserRouter>
+    
   );
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
 }
 
 export default App;
