46c8ea06420a3d371e9358dab89b42973811157b
Hasegawa-Akito <h.akito1999@gmail.com>
2022-04-11 16:33:42 +0900

    delete header camera-icon

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -8,14 +8,14 @@ function Header (){
     if (pageName.indexOf("heart") >= 0){
         icon = (
             // 押されたら画面遷移
-            <NavLink to="/message" exact><i class="heart-icon fa-solid fa-comment"></i></NavLink>
+            <NavLink to="/message" exact><i class="icon fa-solid fa-comment"></i></NavLink>
         );
     }
     else if (pageName.indexOf("message") >= 0){
         
         icon = (
             // 押されたら画面遷移
-            <NavLink to="/heart" exact><i class="heart-icon fas fa-heart"></i></NavLink>     
+            <NavLink to="/heart" exact><i class="icon fas fa-heart"></i></NavLink>     
         );
     }
     
@@ -26,8 +26,7 @@ function Header (){
           <div class="navbar navbar-dark bg-dark shadow-sm">
             <div class="container">
               <a href="/heart" class="navbar-brand d-flex align-items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-                <strong>thanksApp</strong>
+                <strong clas="my-2">thanksApp</strong>
               </a>
 
               { icon }
